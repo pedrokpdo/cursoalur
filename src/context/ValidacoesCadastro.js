2a54b4155dbc1cc44b58a399318f76e074c63527
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-17 16:02:17 -0300

    comitfinal

--- src/context/ValidacoesCadastro.js
+++ src/context/ValidacoesCadastro.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { validarCPF, validarSenha } from "../models/cadastro";
 
-const ValidacoesCadastro =React.createContext();
+const ValidacoesCadastro = React.createContext(
+    {
+        cpf: semValidacao,
+        senha: semValidacao,
+        nome: semValidacao
+    }
+);
 
+function semValidacao(dados) {
+    console.log(dados);
+    return { valido: true, texto: "" }
+}
 export default ValidacoesCadastro;
 
