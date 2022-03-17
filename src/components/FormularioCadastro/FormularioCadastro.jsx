7af9941cbd9373230994bc2e1cfeb4dc101df4b8
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-17 13:10:02 -0300

    renderizaçao condicional

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,36 @@
+import { Typography } from "@material-ui/core";
 import React, { useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(1)
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+      case 2:
+        return <DadosEntrega />
+      default:
+        return <Typography>error</Typography>
+    }
+  }
+
   return (<>
-    <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-    <DadosUsuario />
-    <DadosEntrega />
+    {formularioAtual(etapaAtual)}
+
+
   </>
   );
 }
+
+//    <DadosEntrega />
+//    <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+
+//
 
 export default FormularioCadastro;
