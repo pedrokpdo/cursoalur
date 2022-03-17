0db4ca72f49ddef4c8c2c91da2c480e284d3e8b4
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-17 12:27:20 -0300

    entendendo problema do client

---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,19 @@
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import React from "react";
+
+function DadosUsuario() {
+    return (
+        <form>
+            <TextField variant="outlined"
+                margin="normal"
+                fullWidth id="email" label="email" type="email" />
+            <TextField variant="outlined"
+                margin="normal"
+                fullWidth id="senha" label="senha" type="password" />
+            <Button  variant="contained" color="primary" type="submit">cadastrar</Button>
+        </form>
+    )
+}
+
+export default DadosUsuario;
