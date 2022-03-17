3299b1a329ac1764775efe92844f76ddca6d0024
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-17 13:39:04 -0300

    trocar switch

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,34 +7,24 @@ import DadosUsuario from "./DadosUsuario";
 function FormularioCadastro({ aoEnviar, validarCPF }) {
 
   const [etapaAtual, setEtapaAtual] = useState(0)
+  const formularios = [
+  <DadosUsuario aoEnviar={proximo} />,
+  <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+  <DadosEntrega />]
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1)
+
   }
-  function formularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo}/>
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />
-      case 2:
-        return <DadosEntrega />
-      default:
-        return <Typography>error</Typography>
-    }
-  }
+  
 
   return (<>
-    {formularioAtual(etapaAtual)}
+    {formularios[etapaAtual]}
 
 
   </>
   );
 }
 
-//    <DadosEntrega />
-//    <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-
-//
 
 export default FormularioCadastro;
